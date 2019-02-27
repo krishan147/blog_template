a7fe6de8d0c9e4fa16448907cdce8a8c490dcb87
krishan vadher <krishan.vadher@gmail.com>
2019-02-27 15:34:29 +0000

    template folders added

--- site/run.py
+++ site/run.py
@@ -1,18 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+            {
+                'author':"dog",
+                'title':"cat",
+                "content":"dog"
+            },
+    {
+        'author': "dogg",
+        'title': "horse",
+        "content": "ddog"
+    }
+]
+
 @app.route("/")
-def home():
-    return "home"
+@app.route("/work")
+def work():
+    return render_template('work.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title="Abouto",posts=posts)
 
 @app.route("/contact")
 def contact():
-    return "contact"
-
-
-@app.route("/work")
-def work():
-    return "work"
+    return render_template('contact.html')
 
 if __name__=='__main__':
     app.run(debug=True)
