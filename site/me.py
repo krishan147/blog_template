88bc93f27fc647acd63f4d1a1c5fd1cb92bdc184
krishan vadher <krishan.vadher@gmail.com>
2019-03-04 19:04:24 +0000

    updated me

--- site/me.py
+++ site/me.py
@@ -1,6 +1,6 @@
 def meInfo():
     import json
 
-    parsed = json.load(open('me.json')) # change to add_your_about_info_here.json
+    workDets = json.load(open('me.json')) # change to add_your_about_info_here.json
 
-    return parsed
+    return workDets
--- site/run.py
+++ site/run.py
@@ -4,7 +4,7 @@ from flask_caching import Cache
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-posts = [
+allWork = [
             {
                 'author':"dog",
                 'title':"cat",
@@ -20,18 +20,14 @@ posts = [
 @app.route("/")
 @app.route("/work")
 def work():
-    return render_template('work.html',posts=posts)
+    return render_template('work.html',allWork=allWork)
 
 @app.route("/me")
 def me():
     from me import meInfo
-    parsed = meInfo()
+    meDetails = meInfo()
 
-    name =  parsed["name"]
-    job =  parsed["job"]
-    contact =  parsed["contact"]
-
-    return render_template('me.html',title="Abouto",parsed=parsed)
+    return render_template('me.html',title="Me",meDetails=meDetails)
 
 if __name__=='__main__':
     app.run(debug=True)
