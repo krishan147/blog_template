b636c49f8578e45f8eb3ff252c54ad763c89b4d9
krishan vadher <krishan.vadher@gmail.com>
2019-03-08 17:42:40 +0000

    readme changes

--- site/me.py
+++ site/me.py
@@ -1,6 +1,17 @@
 def meInfo():
     import json
+    from flask_wtf import FlaskForm
+    from wtforms import StringField
+    from wtforms.validators import DataRequired, Length, Email
 
     workDets = json.load(open('me.json')) # change to add_your_about_info_here.json
+
+    # Form
+    class RegistrationForm(FlaskForm):
+        name = StringField('Name', validators=[DataRequired(),Length(min=1,max=250)])
+        emailaddress = StringField('Email Address', validators=[DataRequired(),Email()])
+        message = StringField('Message',Length(min=1,max=2500))
+
+
 
     return workDets
--- tests/1. list comprehenion.py
+++
@@ -1,8 +0,0 @@
-
-listo = [[1,2,3],['a','b','c'],['dog','cat','horse']]
-
-stringo = '23423432'
-
-h_letters = [ letter for letter in listo ]
-
-print (h_letters)
